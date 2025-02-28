7491c73272f7980e73adac3dc24b55d3231a820f
Harishkumaresan <harishkumaresan14@gmail.com>
2025-02-28 17:44:30 +0530

    updates

--- reactfirst/src/Redux/productSaga.js
+++ reactfirst/src/Redux/productSaga.js
@@ -2,13 +2,13 @@ import {takeEvery , put, takeLatest} from 'redux-saga/effects'
 import { PRODUCT_LIST, SEARCH_PRODUCTS, SET_PRODUCT_LIST } from './constant';
 
 function* getProducts(){
-    let data = yield fetch("http://localhost:3500/products")
+    let data = yield fetch("http://localhost:5000/products")
     data = yield data.json();
      console.log("action productlist is called",data)
      yield put({type: SET_PRODUCT_LIST,data})
 }
 function* searchProducts(data){
-    let result = yield fetch(`http://localhost:3500/products?q=${data.query}`)
+    let result = yield fetch(`http://localhost:5000/products?q=${data.query}`)
     result = yield result.json();
      console.log("action productlist is called",result) 
      yield put({type: SET_PRODUCT_LIST,data:result})
